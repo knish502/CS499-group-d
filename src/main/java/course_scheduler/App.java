07d1f746ef1cd652ff9bde347572d796d3511c56
Deoxys0 <evilcreature123@gmail.com>
2021-02-09 15:29:13 -0600

    sarah UI stuff

--- src/main/java/course_scheduler/App.java
+++ src/main/java/course_scheduler/App.java
@@ -9,6 +9,9 @@ public class App {
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        //System.out.println(new App().getGreeting());
+        NewJFrame frame = new NewJFrame();
+        String[] test = {"Test"};
+        frame.main(test);
     }
 }
