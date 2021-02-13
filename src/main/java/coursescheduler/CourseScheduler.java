583260b337b30b7a1c23a07f65dac088ce219642
juwuanturnerhow <juwuanturnerhoward@gmail.com>
2021-02-13 13:40:08 -0600

    TSK-14 Pull checkstyle config changes

--- src/main/java/coursescheduler/CourseScheduler.java
+++ src/main/java/coursescheduler/CourseScheduler.java
@@ -5,5 +5,10 @@ package coursescheduler;
  */
 public class CourseScheduler {
   public static void main(String[] args) {
+      java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LoginPage().setVisible(true);
+            }
+        });
   }
 }
