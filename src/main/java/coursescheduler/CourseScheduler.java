a0be72e2b09376f7e3e78e4f029004769c264206
Juwuan Turner-Howard <juwuanturnerhoward@gmail.com>
2021-02-13 15:44:06 -0600

    TSK-25 Gradle googleJavaFormat setup

--- src/main/java/coursescheduler/CourseScheduler.java
+++ src/main/java/coursescheduler/CourseScheduler.java
@@ -1,9 +1,7 @@
 package coursescheduler;
 
-/**
- * Entry-point for Course Scheduler system.
- */
+/** Entry-point for Course Scheduler system. */
 public class CourseScheduler {
-  public static void main(String[] args) {
-  }
+
+  public static void main(String[] args) {}
 }
