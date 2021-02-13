ab83a61ed2015d55469091a986a91c44c795e420
Juwuan Turner-Howard <juwuanturnerhoward@gmail.com>
2021-02-13 15:44:06 -0600

    TSK-25 Gradle googleJavaFormat setup

--- src/main/java/coursescheduler/CourseScheduler.java
+++ src/main/java/coursescheduler/CourseScheduler.java
@@ -1,14 +1,7 @@
 package coursescheduler;
 
-/**
- * Entry-point for Course Scheduler system.
- */
+/** Entry-point for Course Scheduler system. */
 public class CourseScheduler {
-  public static void main(String[] args) {
-      java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new LoginPage().setVisible(true);
-            }
-        });
-  }
+
+  public static void main(String[] args) {}
 }
