11d1568580f98170d5acbf3230ddfd120c5b36e4
juwuanturnerhow <juwuanturnerhoward@gmail.com>
2021-02-21 12:21:12 -0600

    TSK-29 Merge main branch via rebasing

--- src/main/java/coursescheduler/CourseScheduler.java
+++ src/main/java/coursescheduler/CourseScheduler.java
@@ -22,7 +22,19 @@ public class CourseScheduler {
 =======
 /** Entry-point for Course Scheduler system. */
 public class CourseScheduler {
+<<<<<<< HEAD
 
   public static void main(String[] args) {}
 >>>>>>> TSK-25 Gradle googleJavaFormat setup
+=======
+  public static void main(String[] args) {
+    /* Create and display the form */
+    java.awt.EventQueue.invokeLater(
+        new Runnable() {
+          public void run() {
+            new CourseSchedulerFrame().setVisible(true);
+          }
+        });
+  }
+>>>>>>> TSK-29 Merge main branch via rebasing
 }
