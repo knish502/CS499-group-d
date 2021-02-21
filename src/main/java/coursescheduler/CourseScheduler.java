26bd2cf9545f0b30c0a20b38621919cda6a0275f
juwuanturnerhow <juwuanturnerhoward@gmail.com>
2021-02-21 12:21:12 -0600

    TSK-29 Merge main branch via rebasing

--- src/main/java/coursescheduler/CourseScheduler.java
+++ src/main/java/coursescheduler/CourseScheduler.java
@@ -2,6 +2,13 @@ package coursescheduler;
 
 /** Entry-point for Course Scheduler system. */
 public class CourseScheduler {
-
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    /* Create and display the form */
+    java.awt.EventQueue.invokeLater(
+        new Runnable() {
+          public void run() {
+            new CourseSchedulerFrame().setVisible(true);
+          }
+        });
+  }
 }
